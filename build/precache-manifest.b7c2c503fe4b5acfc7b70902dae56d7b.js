88a15dea781082f68c67d0ff74d4cce28c19d083
christophercalle <christophercalle@gmail.com>
2018-12-21 11:19:11 -0600

    final touches...for now

---
+++ build/precache-manifest.b7c2c503fe4b5acfc7b70902dae56d7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cea10dbcf201aff141c",
+    "url": "/christophercalle/reactredux/static/js/runtime~main.3cea10db.js"
+  },
+  {
+    "revision": "7108a5d1eff729c11aa8",
+    "url": "/christophercalle/reactredux/static/js/main.7108a5d1.chunk.js"
+  },
+  {
+    "revision": "2949b10ee2478be794ea",
+    "url": "/christophercalle/reactredux/static/js/1.2949b10e.chunk.js"
+  },
+  {
+    "revision": "7108a5d1eff729c11aa8",
+    "url": "/christophercalle/reactredux/static/css/main.29d3b1a9.chunk.css"
+  },
+  {
+    "revision": "d1579e8af2c07f3c23fd29b1938b6ac8",
+    "url": "/christophercalle/reactredux/index.html"
+  }
+];
